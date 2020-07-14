d9b73b520416ca3ba698319dbf75b921d48e4cec
Rachael Ghorbani <rachael.ghorbani@gmail.com>
2020-07-14 18:05:02 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+final_results = data.each_with_object({}) do |(key, value), final_hash|
+value.each do |inner_key, inner_array|
+  inner_array.each do |name|
+    if !final_hash[name]
+      final_hash[name] = {}
+    end
+    if !final_hash[name][key] 
+      final_hash[name][key]= []
+    end
+    final_hash[name][key].push(inner_key.to_s)
+    end
+  end
+end
+final_results
 end
