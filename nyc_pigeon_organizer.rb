1395bdbbd3483ecb0c80cc80883365343e931634
Rachael Ghorbani <rachael.ghorbani@gmail.com>
2020-07-14 18:04:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,7 +8,7 @@ value.each do |inner_key, inner_array|
     if !final_hash[name][key] 
       final_hash[name][key]= []
     end
-    final_hash[name][key].push(inner_key)
+    final_hash[name][key].push(inner_key.to_s)
     end
   end
 end
