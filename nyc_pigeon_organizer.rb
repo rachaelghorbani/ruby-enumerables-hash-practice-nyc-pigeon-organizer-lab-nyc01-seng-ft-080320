ec742dbde25235a7cc8fd729b8d4fcba84914715
Rachael Ghorbani <rachael.ghorbani@gmail.com>
2020-07-14 17:56:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,35 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+
+  data.map do |first_key, first_nested_array|
+    first_nested_array.map do |inner_key, inner_array|
+      inner_array.map do |names|
+        new_hash[names] = {}
+      end
+    end
+  end
+  pp new_hash
+
+end
+
+
+
+def nyc_pigeon_organizer(data)
+  new_hash = {}
+
+  data.map do |first_key, first_nested_array|
+    first_nested_array.map do |inner_key, inner_array
+              new_hash[first_key] = inner_array
+||
+      inner_array.map do |names|
+         
+          
+        
+
+      end
+      
+    end
+  end
+    pp new_hash
+
 end
